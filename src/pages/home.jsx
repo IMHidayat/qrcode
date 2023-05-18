eb6c1aed8a5a07cd6d9320106b687b93b6c6921b
Imam Hidayat <imhidayat100602@gmail.com>
2023-05-18 14:34:26 +0700

    QR CODE FINISH

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,9 +4,10 @@ export default function Home() {
   return (
     <>
       <main className="grid place-content-center h-screen bg-main ">
-        <div className="rounded-2xl overflow-hidden m-10 p-5 bg-white">
-          <img className="mb-5 img rounded-2xl bg-red-500" src={qrImage} alt="QR Image" />
-          <h1 className="text-center text-xl font-bold">Improve your front-end skills by building projects</h1>
+        <div className="grid w-[380px] place-items-center rounded-2xl text-center overflow-hidden m-10 p-5 bg-white">
+          <img className="mb-5 img rounded-2xl" src={qrImage} alt="QR Image" />
+          <h1 className="mb-4 mx-4 leading-tight text-[25px] font-bold tracking-tighter text-black">Improve your front-end skills by building projects</h1>
+          <p className="mx-4 mb-4 leading-tight text-[19px] tracking-tight text-gray-500">scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
         </div>
       </main>
     </>
